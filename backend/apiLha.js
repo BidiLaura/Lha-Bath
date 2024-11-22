4dc51dfc9ec993307cb9e2e6449ce73f10eb909e
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-11-22 17:05:10 -0300

    auau

--- backend/apiLha.js
+++ backend/apiLha.js
@@ -30,56 +30,71 @@ app.post('/cadastro', (req, res) => {
 
 // atualizar por ID
 app.put('/atualizar/:id', (req, res) => {
-    const { ID_User } = req.params;
+    const { id } = req.params;
     const { CNPJ, Login, Email, Senha, Nome} = req.body;
     banco.query(
-        `UPDATE User SET CNPJ = ?, Login = ?, Email = ?, Senha = ?, Nome = ? WHERE id = ?`,
-        [CNPJ, Login, Email, Senha, Nome],
+        `UPDATE User SET CNPJ = ?, Login = ?, Email = ?, Senha = ?, Nome = ? WHERE ID_User = ?`,
+        [CNPJ, Login, Email, Senha, Nome, id],
         (err, results) => {
             if (err) {
                 console.error('Erro na atualização', err);
                 return res.status(500).send('Erro na atualização');
             }
-            res.send(`Usuário atualizado:\n\n ID: ${ID_User} \n CNPJ: ${CNPJ} \n Login: ${Login} \n E-mail: ${Email} \n Senha: ${Senha} \n Nome: ${Nome}`);
+            res.send(`Usuário atualizado:\n\n ID: ${id} \n CNPJ: ${CNPJ} \n Login: ${Login} \n E-mail: ${Email} \n Senha: ${Senha} \n Nome: ${Nome}`);
         }
     );
 });
 
-// deletar por ID
-app.delete('/deletarid/:id', (req, res) => {
+// deletar por id
+app.delete('/deletar/:id', (req, res) => {
     const { ID_User } = req.params;
     banco.query(
-        `DELETE FROM veiculos WHERE id = ?`,
-        [Number(id)],
+        `DELETE * FROM User WHERE ID_User = ?`,
+        [Number(ID_User)],
         (err, results) => {
             if (err) {
                 console.error('Ops, erro para deletar.', err);
                 return res.status(500).send('Erro ao deletar');
             }
-            res.send(`Carro deletado com sucesso!`);
+            res.send(`Usuário deletado com sucesso!`);
         }
     );
 });
 
 // selecionar por ID
-app.get('/login/:id', (req, res) => {
-    const { id } = req.params;
+app.post('/Login', (req, res) => {
+    const { Login, Senha } = req.body;
+    console.log(Login, Senha)
     banco.query(
-        `SELECT * FROM User WHERE id = ?`,
-        [Number(id)],
+        `SELECT * FROM User WHERE Login = ? AND Senha = ?`,
+        [Login, Senha],  // Passando os valores de Login e Senha corretamente
         (err, results) => {
             if (err) {
-                console.error('Erro na consulta por id:', err);
-                return res.status(500).json({ error: 'Erro ao consultar usuário por id' });
+                console.error('Erro na consulta por Login:', err);
+                return res.status(500).json({ error: 'Erro ao consultar usuário.' });
             }
             if (results.length === 0) {
                 return res.status(404).send('Usuário não encontrado.');
-              }
-              return res.json(results[0]); // Retorna o primeiro usuário encontrado
             }
+            return res.json(results[0]);
+        }
+    );    
+});
+
+app.get('/', (req, res) => {
+    banco.query(
+        `SELECT * FROM user`,
+        function (err, results, fields) {
+            if (err) {
+                console.error('Erro na consulta:', err);
+                return res.status(500).json({ error: 'Erro ao consultar veículos' });
+            }
+            // Retorna os resultados como um objeto JSON
+            return res.json(results);
+        }
     );
 });
 
 app.listen(port, () => {
-    console.log(`Exemplo de app sendo "escutado" na porta ${port}`);
+    console.log(`Ta funfando na porta ${port} ;)`);
 });
